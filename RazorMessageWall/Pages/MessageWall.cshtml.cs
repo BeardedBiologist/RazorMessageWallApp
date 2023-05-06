4e7ab3e6904ebeb88e563a487271ea100d653af7
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-06 16:29:03 +0200

    init commit

---
+++ RazorMessageWall/Pages/MessageWall.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorMessageWall.Pages
+{
+    public class MessageWallModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
